395c86695532f603eabded4170dfff43361ca277
Pedro Lopes <pfmiralopes@gmail.com>
2016-08-13 18:22:38 +0100

    Objects in the room are now displayed in the details sidebar

--- js/app.js
+++ js/app.js
@@ -1,115 +1,93 @@
 var spaces;
 
 var Module = {
-    TOTAL_MEMORY: 268435456,
-    errorhandler: null, // arguments: err, url, line. This function must return 'true' if the error is handled, otherwise 'false'
-    compatibilitycheck: null,
-    dataUrl: "Release/webgl.data",
-    codeUrl: "Release/webgl.js",
-    memUrl: "Release/webgl.mem",
+  TOTAL_MEMORY: 268435456,
+  errorhandler: null, // arguments: err, url, line. This function must return 'true' if the error is handled, otherwise 'false'
+  compatibilitycheck: null,
+  dataUrl: "Release/webgl.data",
+  codeUrl: "Release/webgl.js",
+  memUrl: "Release/webgl.mem",
 };
 
-
-//Functions to be called form the unity plugin
-function updateFloors(param) {
-    var scope = angular.element($("#controller")).scope();
-    scope.setFloors(param);
+function spacesList(params){
+  var scope = angular.element($("#controller")).scope();
+  scope.setSpaces(params);
 }
 
-function updateSpaces(param) {
-    var scope = angular.element($("#controller")).scope();
-    scope.setSpaces(param);
+function floorsList(params) {
+  var scope = angular.element($("#controller")).scope();
+   scope.setFloors(params);
 }
 
-angular.module("FacilityManager", ['ui.router'])
+angular.module('FacilityManager', ['ui.router'])
 
 .config(function ($stateProvider, $urlRouterProvider) {
-    //
-    // For any unmatched url, redirect to /state1
-    $urlRouterProvider.otherwise("/home");
-    //
-    // Now set up the states
-    $stateProvider
-        .state('home', {
-            url: "/home",
-            templateUrl: "partials/viewer.html",
-            controller: "FacilityManagerController"
-        });
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("/home");
+  // Now set up the states
+  $stateProvider
+    .state('home', {
+        url: "/home",
+        templateUrl: "partials/viewer.html",
+        controller: "FacilityMonitor"
+    });
 })
 
-/*.factory('facilityManagerService', function contentManagerService() {
-    var sidebarName = "";
+.controller('FacilityMonitor', ['$scope', '$timeout', '$rootScope', function ($scope, $timeout, $rootScope) {
+  $scope.spaces = [];
+  $scope.floors = [];
 
-    var exports = {
-        getName: function() {
-            return sidebarName;
-        }, 
-        setName: function(name){
-            console.log("Rui Chamado")
-            sidebarName = name;
-            console.log(sidebarName);
-        }
-    }
-    return exports;
-})*/
+  $scope.setSpaces = function(spaces) {
+    var list = []
 
-
-.controller("FacilityManagerController", ['$scope', '$timeout', '$rootScope', function ($scope, $timeout, $rootScope) {
-    $scope.floors = [];
-    $scope.spaces = [];
-    $rootScope.name = "None";
-    $scope.open = false;
-    
-    //For initializing the tooltips of bootstrap
-    $(function () {
-        $('[data-toggle="tooltip"]').tooltip()
-    })
-
-    $scope.setFloors = function (floors) {
-        $scope.floors = floors;
-    }
-    $scope.setSpaces = function (rooms) {
-        $scope.spaces = rooms;
+    for(var i=0 ; i < spaces.length; i++){
+      list.push(JSON.parse(spaces[i]));
     }
 
-    $scope.getFacilityInformation = function () {
-        SendMessage("ObjectManager", "getSpaces", "");
-        SendMessage("ObjectManager", "getFloors", "");
-    }
+    $scope.spaces = list;
+  }
 
-    $scope.selectLevel = function (level) {
-        SendMessage("ObjectManager", "toggleLevel", level);
-    }
+  $scope.setFloors = function (floors) {
+    $scope.floors = floors;
+  }
 
-    $scope.selectRoom = function (room) {
-        $("#sidebar-right").addClass("active");
-        $scope.open = true;
-        $rootScope.name = room;
-    }
+  $scope.selectLevel = function (level) {
+    SendMessage("ObjectManager", "toggleLevel", level);
+  }
 
+  $scope.selectRoom = function (space) {
+    $("#sidebar-right").addClass("active");
+    $scope.open = true;
+    $rootScope.name = space.spaceName;
+    $rootScope.objects = space.objects;
+  }
 
-    $scope.isSidebarOpen = function () {
-        return open;
-    }
+  $scope.isSidebarOpen = function () {
+    return open;
+  }
 
-    $scope.closeSidebar = function () {
-        $('#sidebar-right').removeClass('active');
-        $scope.open = false;
+  $scope.closeSidebar = function () {
+      $('#sidebar-right').removeClass('active');
+      $scope.open = false;
+  }
 
-    }
+  $scope.setOrbitMode = function () {
+      console.log("Nvaigation mode: orbit");
+      SendMessage("ObjectManager", "orbitMode", "");
+  }
 
-    $scope.setOrbitMode = function () {
-        console.log("Nvaigation mode: orbit");
-        SendMessage("ObjectManager", "orbitMode", "");
-    }
+  $scope.setNormalMode = function () {
+      console.log("Navigation mode: normal");
+      SendMessage("ObjectManager", "normalMode", "");
+  }
 
-    $scope.setNormalMode = function () {
-        console.log("Navigation mode: normal");
-        SendMessage("ObjectManager", "normalMode", "");
-    }
+  $timeout(function () {
+    SendMessage("ObjectManager", "availableFloors", "");
+    SendMessage("ObjectManager", "availableSpaces", "");
+  }, 20000)
 
-    $timeout(function () {
-        $scope.getFacilityInformation();
-    }, 30000)
-
+  //For initializing the tooltips of bootstrap
+  $(function () {
+     $('[data-toggle="tooltip"]').tooltip()
+ })
 }]);
