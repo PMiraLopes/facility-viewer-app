5b4ccbfada97c0703adac02bcce547abe10c068e
Pedro Lopes <pfmiralopes@gmail.com>
2016-08-28 16:34:49 +0100

    Add boxes view, some new elements to the design, and start to build the grpahs dashboard

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@ function floorsList(params) {
    scope.setFloors(params);
 }
 
-angular.module('FacilityManager', ['ui.router'])
+angular.module('FacilityManager', ['ui.router', 'chart.js', 'ngMaterial'])
 
 .config(function ($stateProvider, $urlRouterProvider) {
   // For any unmatched url, redirect to /state1
@@ -31,9 +31,16 @@ angular.module('FacilityManager', ['ui.router'])
         templateUrl: "partials/viewer.html",
         controller: "FacilityMonitor"
     });
+
+  $stateProvider
+    .state('dashboard', {
+        url: "/dashboard",
+        templateUrl: "partials/dashboard.html",
+        controller: "FacilityMonitor"
+    });
 })
 
-.controller('FacilityMonitor', ['$scope', '$timeout', '$rootScope', function ($scope, $timeout, $rootScope) {
+.controller('FacilityMonitor', ['$scope', '$timeout', '$rootScope', '$mdSidenav', function ($scope, $timeout, $rootScope, $mdSidenav) {
   $scope.spaces = [];
   $scope.floors = [];
 
@@ -55,16 +62,18 @@ angular.module('FacilityManager', ['ui.router'])
     SendMessage("ObjectManager", "toggleLevel", level);
   }
 
+  $scope.toggleBoxesView = function () {
+    SendMessage("ObjectManager", "toggleBoxesView", "");
+  }
+
   $scope.selectRoom = function (space) {
-    $("#sidebar-right").addClass("active");
+  //  $("#sidebar-right").addClass("active");
+    if(!$scope.open)
+      $scope.toggleRight();
     $scope.open = true;
     $rootScope.name = space.spaceName;
     $rootScope.objects = space.objects;
     SendMessage("ObjectManager", "selectSpace", space.spaceName);
-  }
-
-  $scope.isSidebarOpen = function () {
-    return open;
   }
 
   $scope.closeSidebar = function () {
@@ -90,5 +99,56 @@ angular.module('FacilityManager', ['ui.router'])
   //For initializing the tooltips of bootstrap
   $(function () {
      $('[data-toggle="tooltip"]').tooltip()
- })
+   });
+
+   /////////////////////LINE CHART//////////////////
+
+   $scope.labels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"];
+  $scope.series = ['Series A', 'Series B'];
+  $scope.data = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+  $scope.onClick = function (points, evt) {
+    console.log(points, evt);
+  };
+  $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+  $scope.options = {
+    scales: {
+      yAxes: [
+        {
+          id: 'y-axis-1',
+          type: 'linear',
+          display: true,
+          position: 'left'
+        },
+        {
+          id: 'y-axis-2',
+          type: 'linear',
+          display: true,
+          position: 'right'
+        }
+      ]
+    }
+  };
+
+  ///////////////////////PIE CHART/////////////////
+
+  $scope.barLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  $scope.barSeries = ['Series A'];
+
+  $scope.barData = [
+    [65, 59, 80, 81, 56, 55, 40]
+  ];
+
+  ///////// SIDEBAR /////////
+  $scope.toggleLeft = buildToggler('left');
+  $scope.toggleRight = buildToggler('right');
+
+  function buildToggler(componentId) {
+    return function() {
+      $mdSidenav(componentId).toggle();
+    }
+  }
+
 }]);
