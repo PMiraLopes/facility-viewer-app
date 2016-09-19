9836be12149d9aeee2e284696925287b13f97146
Pedro Lopes <pfmiralopes@gmail.com>
2016-09-19 22:45:50 +0100

    Add daily chart view in dashboard

--- js/controllers/dashboard.js
+++ js/controllers/dashboard.js
@@ -31,6 +31,7 @@ angular.module('FacilityManager.dashboard', ['ui.router', 'chart.js', 'ngMateria
 
 
   $scope.months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  $scope.days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
   $scope.consumptionsSeries = ['Consumption (kWh)'];
   $scope.costsSeries = ['Costs (€)'];
 
@@ -78,14 +79,67 @@ angular.module('FacilityManager.dashboard', ['ui.router', 'chart.js', 'ngMateria
   $scope.monthsCompareCostsLine = [[],[]];
   $scope.monthsCompareCostsLineOptions = {
     title: {
-      text: "Monthly comparition of costs (€)",
+      text: "Monthly costs comparition (€)",
       display: true,
       padding: 10
     }
   }
   $scope.monthsCompareConsumptionLineOptions = {
     title: {
-      text: "Monthly comparition of consumption (kWh)",
+      text: "Monthly consumption comparition (kWh)",
+      display: true,
+      padding: 10
+    }
+  }
+  $scope.monthsCompareConsumptionSeries = ["2016's monthly compsumption (kWh)", "2015's monthly compsumption (kWh)"]
+
+
+  $scope.pieChartLabels = ['Developers', 'Designers', 'Meeting', 'Wc 1', 'Wc 2', 'Wc 3'];
+  $scope.pieYearOptions = {
+    title: {
+      text: "2016's consumption by room (kWh)",
+      display: true,
+      padding: 10
+    }
+  }
+
+
+//////////// DAYS /////////////////////
+  $scope.dailyCostsLineData = [[],[]];
+  $scope.dailyCostSeries = ["1st month daily costs", "2nd month daily costs"]
+  $scope.dailyCostsOptions = {
+    title: {
+      text: "Daily costs comparition (€)",
+      display: true,
+      padding: 10
+    }
+  }
+
+  $scope.dailyConsumptionLineData = [[],[]];
+  $scope.dailyConsumptionSeries = ["1st month daily compsumption", "2nd month daily compsumption"]
+  $scope.dailyConsumptionOptions = {
+    title: {
+      text: "Daily consumption comparition (kWh)",
+      display: true,
+      padding: 10
+    }
+  }
+
+  $scope.dailyCurrentConsumptionLineData = [[]];
+  $scope.dailyCurrentConsumptionSeries = ["Daily compsumption"]
+  $scope.dailyCurrentConsumptionOptions = {
+    title: {
+      text: "Current month daily consumption",
+      display: true,
+      padding: 10
+    }
+  }
+
+  $scope.dailyCurrentCostsLineData = [[]];
+  $scope.dailyCurrentCostsSeries = ["Daily costs"]
+  $scope.dailyCurrentCostsOptions = {
+    title: {
+      text: "Current month daily costs",
       display: true,
       padding: 10
     }
@@ -95,6 +149,18 @@ angular.module('FacilityManager.dashboard', ['ui.router', 'chart.js', 'ngMateria
   $scope.applyYears = function () {
     var startYearData = $scope.getTotalDataFromYear($scope.dates.startYearDate.getFullYear());
     var endYearData = $scope.getTotalDataFromYear($scope.dates.endYearDate.getFullYear());
+
+    var startMonthData = $scope.getDataFromMonth($scope.dates.startYearDate.getFullYear(), $scope.dates.startYearDate.getMonth());
+    var endMonthData = $scope.getDataFromMonth($scope.dates.endYearDate.getFullYear(), $scope.dates.endYearDate.getMonth());
+
+    $scope.pieYearData = [
+      Math.round((endYearData * 35) / 100),
+      Math.round((endYearData * 30) / 100),
+      Math.round((endYearData * 22) / 100),
+      Math.round((endYearData * 5) / 100),
+      Math.round((endYearData * 5) / 100),
+      Math.round((endYearData * 3) / 100)
+    ];
 
     $scope.yearBarConsumption = [ startYearData, endYearData];
     $scope.yearBarCosts = [
@@ -113,6 +179,15 @@ angular.module('FacilityManager.dashboard', ['ui.router', 'chart.js', 'ngMateria
     $scope.monthlyConsumptionResults = endYearData/($scope.currentMonth + 1);
     $scope.monthlyCostsResults = (endYearData * $scope.price)/($scope.currentMonth + 1);
 
+    $scope.dailyConsumptionLineData[0] = startMonthData.data;
+    $scope.dailyConsumptionLineData[1] = endMonthData.data;
+
+    $scope.dailyCostsLineData[0] = startMonthData.data.map(function(x) { return x * $scope.price; });
+    $scope.dailyCostsLineData[1] = endMonthData.data.map(function(x) { return x * $scope.price; });
+
+    $scope.averageMonthResults = Math.round(((startMonthData.average - endMonthData.average) * 100)/ startMonthData.average);
+    $scope.monthCosts = Math.round((endMonthData.total - startMonthData.total) * $scope.price);
+
   }
 
 
@@ -130,6 +205,17 @@ angular.module('FacilityManager.dashboard', ['ui.router', 'chart.js', 'ngMateria
         endYearData * $scope.price
       ];
 
+      console.log(endYearData);
+
+      $scope.pieYearData = [
+        Math.round((endYearData * 35) / 100),
+        Math.round((endYearData * 30) / 100),
+        Math.round((endYearData * 22) / 100),
+        Math.round((endYearData * 5) / 100),
+        Math.round((endYearData * 5) / 100),
+        Math.round((endYearData * 3) / 100)
+      ];
+
       $scope.yearResults = Math.round(((endYearData - startYearData) * 100)/ startYearData);
       $scope.yearCosts = Math.round((endYearData - startYearData) * $scope.price);
 
@@ -145,36 +231,10 @@ angular.module('FacilityManager.dashboard', ['ui.router', 'chart.js', 'ngMateria
         $scope.monthsCompareCostsLine[1].push($scope.data.years[0].data[i].total * $scope.price);
       }
 
-      $scope.monthlyConsumptionResults = endYearData/($scope.currentMonth + 1);
-      $scope.monthlyCostsResults = (endYearData * $scope.price)/($scope.currentMonth + 1);
+      var currentMonthData = $scope.getDataFromMonth($scope.currentYear, $scope.currentMonth);
 
-    /*  for (var i = 0; i < data.years.length; i++){
-        $scope.barLabels.push(data.years[i].year);
-        $scope.barData[0].push(data.years[i].total);
-        var yearData = [];
-
-        for (var k = 0; k < data.years[i].data.length; k++){
-          yearData.push(data.years[i].data[k].total);
-        }
-
-        if(i < 1){
-        //  $scope.chartData.push(yearData);
-          $scope.pieChartData = [
-            data.years[0].data[0].total * (2/6),
-            data.years[0].data[0].total * (2/6),
-            data.years[0].data[0].total * (1/6),
-            data.years[0].data[0].total * (0.4/6),
-            data.years[0].data[0].total * (0.3/6),
-            data.years[0].data[0].total * (0.3/6)
-          ];
-        }
-        console.log($scope.chartData[0]);
-        for( var j = 0; j < $scope.chartData[0].length; j++){
-          $scope.priceChartData[0].push($scope.chartData[0][j] * $scope.price);
-        }
-        console.log($scope.priceChartData);
-        $scope.yearsChartData.push(yearData);
-      }*/
+      $scope.dailyCurrentConsumptionLineData[0] = currentMonthData.data;
+      $scope.dailyCurrentCostsLineData[0] = currentMonthData.data.map(function(x) { return x * $scope.price; });
     })
     .error(function(data){
       console.log('Error: ' + data);
@@ -204,69 +264,13 @@ angular.module('FacilityManager.dashboard', ['ui.router', 'chart.js', 'ngMateria
     return 0;
   };
 
-/*
-
-  /*$scope.barLabels = [2015, 2016];
-  $scope.barData = [$scope.getTotalDataFromYear($scope.defaultMinYear), $scope.getTotalDataFromYear($scope.defaultMaxYear)];
-  $scope.barSeries = ['Consumption (kWh)'];
-  $scope.barOptions = {
-    title: {
-      text: "Years overall electric consumptions",
-      display: true,
-      padding: 10
+  $scope.getDataFromMonth = function(year, month){
+    console.log(year, month)
+    for(var i = 0; $scope.data.years.length; i++){
+      if($scope.data.years[i].year === year ){
+        return $scope.data.years[i].data[month];
+      }
     }
-  }
-
-  $scope.chartLabels = $scope.months;
-  $scope.chartData = [];
-  $scope.chartSeries = ['Consumption (kWh)'];
-  $scope.chartOptions = {
-    title: {
-      text: "Monthly electric consumption",
-      display: true,
-      padding: 10
-    }
-  }
-
-  $scope.yearsChartData = [$scope.defaultMinYear, $scope.defaultMaxYear];
-  $scope.yearsChartSeries = ['2015\'s consumption', '2016\'s consumption'];
-  $scope.yearsChartOptions = {
-    title: {
-      text: "Month compare of electric consumption",
-      display: true,
-      padding: 10
-    }
-  }
-
-  $scope.pieChartData = [];
-  $scope.pieChartLabels = ['Developers', 'Designers', 'Meeting', 'Wc 1', 'Wc 2', 'Wc 3'];
-  $scope.pieChartOptions = {
-    title: {
-      text: "Monthly room's percentage of electric consumption",
-      display: true,
-      padding: 10
-    }
-  }
-
-  $scope.priceChartLabels = $scope.months;
-  $scope.priceChartData = [[]];
-  $scope.priceChartSeries = ['Costs (€)'];
-  $scope.priceChartOptions = {
-    title: {
-      text: "Monthly costs of electric consumption",
-      display: true,
-      padding: 10
-    }
-  }
-
-  ///////// SIDEBAR /////////
-  $scope.toggleLeft = buildToggler('left');
-  $scope.toggleRight = buildToggler('right');
-
-  function buildToggler(componentId) {
-    return function() {
-      $mdSidenav(componentId).toggle();
-    }
-  }*/
-
+    return [];
+  };
 }]);
